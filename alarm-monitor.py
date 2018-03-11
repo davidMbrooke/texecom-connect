825c9580034d75f4bdcb28d14f5896d494332dbf
Joseph Heenan <joseph@heenan.me.uk>
2018-03-11 15:08:55 +0000

    Refactor to have a message handler

--- alarm-monitor.py
+++ alarm-monitor.py
@@ -48,20 +48,23 @@ class TexecomConnect:
     MSG_USEREVENT = chr(4)
     MSG_LOGEVENT = chr(5)
     
-    def __init__(self, host, port):
+    def __init__(self, host, port , message_handler_func):
         self.host = host
         self.port = port
         self.crc8_func = crcmod.mkCrcFun(poly=0x185, rev=False, initCrc=0xff)
         self.nextseq = 0
+        self.message_handler_func = message_handler_func
 
     def hexstr(self,s):
         return " ".join("{:02x}".format(ord(c)) for c in s)
 
     def connect(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         self.s.connect((self.host, self.port))
-        time.sleep(0.4)
+        # if we send the login message to fast the panel ignores it; texecom
+        # recommend 500ms, see:
+        # http://texecom.websitetoolbox.com/post/show_single_post?pid=1303528828&postcount=4&forum=627911
+        time.sleep(0.5)
         
     def getnextseq(self):
         if self.nextseq == 256:
@@ -69,26 +72,34 @@ class TexecomConnect:
         next=self.nextseq
         self.nextseq += 1
         return next
+
     
-    def recvpacket(self):
-        header = self.s.recv(self.LENGTH_HEADER)
-        #hexdump.hexdump(header)
-        msg_start,msg_type,msg_length,msg_sequence = list(header)
-        payload = self.s.recv(ord(msg_length) - self.LENGTH_HEADER)
-        payload, msg_crc = payload[:-1], ord(payload[-1])
-        expected_crc = self.crc8_func(header+payload)
-        if msg_start != 't':
-            print("unexpected msg start: "+hex(ord(msg_start)))
-        # FIXME: check seq
-        # FIXME: should probably make sure this isn't an 'M' message
-        # FIXME: check we received the full expected length
-        # FIXME: add a timeout to recv(), if panel takes over 1second to finish sending something is probably wrong
-        if msg_crc != expected_crc:
-            print("crc: expected="+str(expected_crc)+" actual="+str(msg_crc))
-        return payload
+    def recvresponse(self):
+        """Receive a response to a command. Automatically handles any
+        messages that arrive first"""
+        while True:
+            header = self.s.recv(self.LENGTH_HEADER)
+            #hexdump.hexdump(header)
+            msg_start,msg_type,msg_length,msg_sequence = list(header)
+            payload = self.s.recv(ord(msg_length) - self.LENGTH_HEADER)
+            payload, msg_crc = payload[:-1], ord(payload[-1])
+            expected_crc = self.crc8_func(header+payload)
+            if msg_start != 't':
+                print("unexpected msg start: "+hex(ord(msg_start)))
+            if msg_crc != expected_crc:
+                print("crc: expected="+str(expected_crc)+" actual="+str(msg_crc))
+            # FIXME: check seq
+            # FIXME: check we received the full expected length
+            # FIXME: add a timeout to recv(), if panel takes over 1second to finish sending something is probably wrong
+            if msg_type == self.HEADER_TYPE_COMMAND:
+                print("received command unexpectedly")
+            elif msg_type == self.HEADER_TYPE_RESPONSE:
+                return payload
+            elif msg_type == self.HEADER_TYPE_MESSAGE:
+                self.message_handler_func(payload)
     
     def sendcommand(self, body):
-        data = 'tC'+chr(len(body)+5)+chr(self.getnextseq())+body
+        data = self.HEADER_START+self.HEADER_TYPE_COMMAND+chr(len(body)+5)+chr(self.getnextseq())+body
         data += chr(self.crc8_func(data))
         # hexdump.hexdump(data)
         self.s.send(data)
@@ -96,7 +107,7 @@ class TexecomConnect:
     def login(self, udl):
         body = self.CMD_LOGIN+udl
         self.sendcommand(body)
-        payload=self.recvpacket()
+        payload=self.recvresponse()
         print("login response payload is: "+self.hexstr(payload))
         commandid,response = list(payload)
         if commandid != self.CMD_LOGIN:
@@ -110,7 +121,7 @@ class TexecomConnect:
         # this enables all messages
         body = self.CMD_SETEVENTMESSAGES+chr(0xff)+chr(0xff)
         self.sendcommand(body)
-        payload=self.recvpacket()
+        payload=self.recvresponse()
         print("set event messages response payload is: "+self.hexstr(payload))
         commandid,response = list(payload)
         if commandid != self.CMD_SETEVENTMESSAGES:
@@ -123,82 +134,84 @@ class TexecomConnect:
     def get_date_time(self):
         body = self.CMD_GETDATETIME
         self.sendcommand(body)
-        payload=self.recvpacket()
+        payload=self.recvresponse()
         commandid,datetime = payload[0],payload[1:]
         if commandid != self.CMD_GETDATETIME:
-            print("Got response for wrong command id: "+hex(ord(commandid)))
+            print("Got response for wrong command id: Expected "+hex(ord(self.CMD_GETDATETIME))+", got "+hex(ord(commandid)))
+            print("Payload: "+self.hexstr(payload))
         datetime = bytearray(datetime)
         datetimestr = '20{2:02d}/{1:02d}/{0:02d} {3:02d}:{4:02d}:{5:02d}'.format(*datetime)
         print("Panel date/time: "+datetimestr)
+    
+    def event_loop(self):
+        while True:
+            try:
+                payload = tc.recvresponse()
+        
+            except socket.timeout:
+                # send any message to reset the panel's 60 second timeout
+                tc.get_date_time()
 
+    def debug_print_message(self, payload):
+        msg_type,payload = payload[0],payload[1:]
+        if msg_type == tc.MSG_DEBUG:
+            print("Debug message: "+tc.hexstr(payload))
+        elif msg_type == tc.MSG_ZONEEVENT:
+            if len(payload) == 2:
+                zone_number = ord(payload[0])
+                zone_bitmap = ord(payload[1])
+            elif len(payload) == 3:
+                zone_number = ord(payload[0])+(ord(payload[1])<<8)
+                zone_bitmap = ord(payload[2])
+            else:
+                print("unknown payload length")
+            zone_state = zone_bitmap & 0x3
+            zone_str = ["secure","active","tamper","short"][zone_bitmap & 0x3]
+            if zone_bitmap & (1 << 2):
+                zone_str += ", fault"
+            if zone_bitmap & (1 << 3):
+                zone_str += ", failed test"
+            if zone_bitmap & (1 << 4):
+                zone_str += ", alarmed"
+            if zone_bitmap & (1 << 5):
+                zone_str += ", manual bypassed"
+            if zone_bitmap & (1 << 6):
+                zone_str += ", auto bypassed"
+            if zone_bitmap & (1 << 7):
+                zone_str += ", zone masked"
+            print("Zone event message: zone "+str(zone_number)+": "+zone_str)
+        elif msg_type == tc.MSG_AREAEVENT:
+            area_number = ord(payload[0])
+            area_state = ord(payload[1])
+            area_state_str = ["disarmed", "in exit", "in entry", "armed", "part armed", "in alarm"][area_state]
+            print("Area event message: area "+str(area_number)+" "+area_state_str)
+        elif msg_type == tc.MSG_OUTPUTEVENT:
+            output_location = ord(payload[0])
+            output_state = ord(payload[1])
+            print("Output event message: location {:d} now 0x{:02x}".
+              format(output_location, output_state))
+        elif msg_type == tc.MSG_USEREVENT:
+            user_number = ord(payload[0])
+            user_state = ord(payload[1])
+            user_state_str = ["code", "tag", "code+tag"][user_state]
+            print("User event message: logon by user {:d} {}".
+              format(user_number, user_state_str))
+        elif msg_type == tc.MSG_LOGEVENT:
+            print("Log event message: "+tc.hexstr(payload))
+        else:
+            print("unknown message type "+str(ord(msg_type))+": "+tc.hexstr(payload))
+
+def message_handler(payload):
+    tc.debug_print_message(payload)
 
 if __name__ == '__main__':
     texhost = '192.168.1.9'
     port = 10001
-    tc = TexecomConnect(texhost, port)
+    tc = TexecomConnect(texhost, port, message_handler)
     tc.connect()
     tc.login('1234')
     print("login successful")
     tc.set_event_messages()
     tc.get_date_time()
     tc.s.settimeout(30)
-    while True:
-        try:
-            # FIXME: instead of doing this, have a callback when an
-            # unsolicited message is received - that way we can cope
-            # with messages that arrive when we're awaiting a command
-            # response
-            payload = tc.recvpacket()
-            msg_type,payload = payload[0],payload[1:]
-            if msg_type == tc.MSG_DEBUG:
-                print("Debug message: "+tc.hexstr(payload))
-            elif msg_type == tc.MSG_ZONEEVENT:
-                if len(payload) == 2:
-                    zone_number = ord(payload[0])
-                    zone_bitmap = ord(payload[1])
-                elif len(payload) == 3:
-                    zone_number = ord(payload[0])+(ord(payload[1])<<8)
-                    zone_bitmap = ord(payload[2])
-                else:
-                    print("unknown payload length")
-                zone_state = zone_bitmap & 0x3
-                zone_str = ["secure","active","tamper","short"][zone_bitmap & 0x3]
-                if zone_bitmap & (1 << 2):
-                    zone_str += ", fault"
-                if zone_bitmap & (1 << 3):
-                    zone_str += ", failed test"
-                if zone_bitmap & (1 << 4):
-                    zone_str += ", alarmed"
-                if zone_bitmap & (1 << 5):
-                    zone_str += ", manual bypassed"
-                if zone_bitmap & (1 << 6):
-                    zone_str += ", auto bypassed"
-                if zone_bitmap & (1 << 7):
-                    zone_str += ", zone masked"
-                print("Zone event message: zone "+str(zone_number)+": "+zone_str)
-            elif msg_type == tc.MSG_AREAEVENT:
-                area_number = ord(payload[0])
-                area_state = ord(payload[1])
-                area_state_str = ["disarmed", "in exit", "in entry", "armed", "part armed", "in alarm"][area_state]
-                print("Area event message: area "+str(area_number)+" "+area_state_str)
-            elif msg_type == tc.MSG_OUTPUTEVENT:
-                output_location = ord(payload[0])
-                output_state = ord(payload[1])
-                print("Output event message: location {:d} now 0x{:02x}".
-                  format(output_location, output_state))
-            elif msg_type == tc.MSG_USEREVENT:
-                user_number = ord(payload[0])
-                user_state = ord(payload[1])
-                user_state_str = ["code", "tag", "code+tag"][user_state]
-                print("User event message: logon by user {:d} {}".
-                  format(user_number, user_state_str))
-            elif msg_type == tc.MSG_LOGEVENT:
-                print("Log event message: "+tc.hexstr(payload))
-            else:
-                print("unknown message type "+str(ord(msg_type))+": "+tc.hexstr(payload))
-
-        except socket.timeout:
-            # send any message to reset the panel's 60 second timeout
-            tc.get_date_time()
-        
-    
+    tc.event_loop()
